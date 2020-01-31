ae382230109a0f0efadaa8ad10d7748522d0c986
wj-soft <wooju_lee@daum.net>
2020-01-31 12:55:32 +0900

    react/docs 튜토리얼

--- src/components/atoms/WButton/index.js
+++ src/components/atoms/WButton/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const Button = styled.button`
   background: ${props => props.primary ? "palevioletred" : "white"};
@@ -12,8 +13,22 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
+/**
+ * 사용법에 대한 설명
+ *
+ * - `test` 컴퍼넌트입니다.
+ */
 function PButton(props) {
   return <Button {...props}>{props.children}</Button>
 }
+
+PButton.propTypes = {
+  /** test docs */
+  value: PropTypes.string,
+};
+
+PButton.defaultProps = {
+  value: 'helloworld'
+};
 
 export default PButton;
