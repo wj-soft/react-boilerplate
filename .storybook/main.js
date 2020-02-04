affa0974e03ec1a934f2710fa574e1e3dca60225
wj-soft <wooju_lee@daum.net>
2020-02-04 21:44:51 +0900

    storybook 설정수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
-    '@storybook/addon-viewport/register'
+    '@storybook/addon-viewport/register',
     '@storybook/addon-docs'
   ]
 };
