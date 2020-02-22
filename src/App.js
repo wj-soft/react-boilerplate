b09b64626caa5fd480f4c95f4ba8ecbbbf91638c
wj-soft <wooju_lee@daum.net>
2020-02-22 20:24:06 +0900

    예제 컴퍼넌트 제거

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      Hello World
-      <Button>Hello</Button>
     </div>
   );
 }
