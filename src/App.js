00bf3e17f7bf6ea6e345ad5297322d5521492c4d
wj-soft <wooju_lee@daum.net>
2020-02-22 15:13:26 +0900

    ant 디자인

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <Button>Hello</Button>
     </div>
   );
 }
